01025a64e111f133efd00951cd67bbfa2dd34f54
Junjie Zhu <84182340+Junjie-Zhu@users.noreply.github.com>
2024-02-26 16:46:18 +0800

    Update latest version, based on Str2Str

--- model/equiformer/__init__.py
+++
@@ -1,9 +0,0 @@
-from .registry import model_entrypoint
-
-from .graph_attention_transformer import *
-from .graph_attention_transformer_md17 import *
-from .graph_attention_transformer_oc20 import *
-
-from .dp_attention_transformer import *
-from .dp_attention_transformer_md17 import *
-from .dp_attention_transformer_oc20 import *
